37929766c03f7faa6c9150b0b1223a898f910173
Kirill <kirilldd2@gmail.com>
2021-07-21 14:29:14 +0700

    added twig templates;

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * Main page controller
      *
      */
-    public function getHomepage()
+    public function getHomepage(): Response
     {
-        return new Response('Hello Symfony');
+        return $this->render('pages/main.html.twig', ['hello' => 'Hellow from controller']);
+    }
+
+    public function getQuestion(string $question): Response
+    {
+        return $this->render('pages/questions.html.twig', [
+            'question' => $question,
+            'questions' => [
+                'How to build a boat? ⛵',
+                'Is Math related to science? 🔬',
+                '2+2=5 confirmed? 🤣',
+            ],
+        ]);
     }
 }
