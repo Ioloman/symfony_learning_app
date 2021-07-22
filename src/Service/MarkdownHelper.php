2f92dc520b56f54082ba410682848587f02415e4
Kirill <kirilldd2@gmail.com>
2021-07-22 16:12:43 +0700

    working with services, commands and twig filters;

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class MarkdownHelper
+{
+    private $markdownParser;
+    private $cache;
+    private $isDebug;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct (MarkdownParserInterface $parser, CacheInterface $cache, bool $isDebug, LoggerInterface $logger)
+    {
+        $this->markdownParser = $parser;
+        $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger = $logger;
+    }
+
+    public function parse(string $text)
+    {
+        if (stripos($text, 'text') !== false) {
+            $this->logger->info('Gotcha!');
+        }
+
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($text);
+        } else {
+            return $this->cache->get('markdown_'.md5($text), function () use ($text) {
+                return $this->markdownParser->transformMarkdown($text);
+            });
+        }
+    }
+}
